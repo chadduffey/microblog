ae2116eb16658147b75009e4b7b36c08c3592298
Chad Duffey <chadduffey@hotmail.com>
2018-01-01 15:12:39 -0800

    user login flow complete

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,5 @@
-from flask import render_template, flash, redirect, url_for
-from flask_login import current_user, login_user, logout_user
+from flask import render_template, flash, redirect, url_for, request
+from flask_login import current_user, login_user, logout_user, login_required
 
 from app import app
 from app.forms import LoginForm
@@ -8,6 +8,7 @@ from app.models import User
 
 @app.route('/')
 @app.route('/index')
+@login_required
 def index():
 	user = {'username':'Chad'}
 	posts = [
@@ -20,7 +21,7 @@ def index():
 			'body' : 'Looking forward to Star Wars!'
 		}
 	]
-	return render_template('index.html', title='Home', user=user, posts= posts)
+	return render_template('index.html', title='Home', posts= posts)
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
@@ -33,7 +34,10 @@ def login():
 			flash('Invalid username or password')
 			return redirect(url_for('login'))
 		login_user(user, remember=form.remember_me.data)
-		return redirect(url_for('index'))
+		next_page = request.args.get('next')
+		if not next_page or not next_page.startswith('/'):
+			next_page = url_for('index')
+		return redirect(next_page)
 	return render_template('login.html', title='Sign In', form=form)
 
 @app.route('/logout')
